a2c6bba670c64236d8c16455eb290e2616d81065
coadyg <graham.coady@gmail.com>
2026-03-19 15:14:15 +0100

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,12 +3,20 @@ import { useEffect } from "react"
 function Login() {
 
   useEffect(() => {
+
     if (window.catalyst) {
+
       window.catalyst.auth.signIn("catalyst-login-container")
+
     }
+
   }, [])
 
-  return <div id="catalyst-login-container"></div>
+  return (
+    <div style={{ paddingTop: "120px", textAlign: "center" }}>
+      <div id="catalyst-login-container"></div>
+    </div>
+  )
 
 }
 
